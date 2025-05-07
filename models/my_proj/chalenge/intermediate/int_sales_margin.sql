ba0e18c2cc2da842d512696134c4bf486d8af5d0
manuelmartins.da.lis.apr25@gmail.com <manuelmartins.da.lis.apr25@gmail.com>
2025-05-07 15:49:03 +0000

    demo class v4

--- models/my_proj/chalenge/intermediate/int_sales_margin.sql
+++ models/my_proj/chalenge/intermediate/int_sales_margin.sql
@@ -8,7 +8,8 @@ SELECT
     quantity,
     purchase_price,
     ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-    ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+    ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin,
+    {{ margin_percent('s.revenue', 's.quantity * CAST(p.purchase_price AS FLOAT64)') }} as margin_percent
 FROM {{ref("stg_raw_chlg__sales")}} s
 LEFT JOIN {{ref("stg_raw_chlg__product")}} p
     USING (products_id)
